b653005400cbebab76586596f5f7e240aff58cfe
Michael Bailey <michael.bailey@crypsisgroup.com>
2020-07-15 19:50:57 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo Building
 export BIN_FILE=$(echo $1|sed 's/.go/_amd64\.bin/g'|sed 's/\///g'|sed 's/\.//g')
 export WIN_FILE=$(echo $1|sed 's/.go/_amd64\.exe/g'|sed 's/\///g'|sed 's/\.//g')
 export MAC_FILE=$(echo $1|sed 's/.go/_amd64_mac\.bin/g'|sed 's/\///g'|sed 's/\.//g')
