e173357426e523dd35619988edff556341c31c66
Michael Bailey <michael.bailey@crypsisgroup.com>
2020-07-15 19:44:47 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 export BIN_FILE=$(echo $1|sed 's/.go/_amd64.bin/g')
 export WIN_FILE=$(echo $1|sed 's/.go/_amd64.exe/g')
 export MAC_FILE=$(echo $1|sed 's/.go/_amd64_mac.bin/g')
-echo go build GOOS=linux GOARCH=amd64 -o $BIN_FILE $1
-echo go build GOOS=windows GOARCH=amd64 -o $WIN_FILE $1
-echo go build GOOS=darwin GOARCH=amd64 -o $MAC_FILE $1
+echo go build GOOS=linux GOARCH=amd64 -o artifacts/$BIN_FILE $1
+echo go build GOOS=windows GOARCH=amd64 -o artifacts/$WIN_FILE $1
+echo go build GOOS=darwin GOARCH=amd64 -o artifacts/$MAC_FILE $1
